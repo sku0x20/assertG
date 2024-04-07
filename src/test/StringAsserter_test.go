60847eae5b54040d1dbbf50e2ddce1550c9897a3
siddhant <siddhantkumarupmanyu@gmail.com>
2024-04-07 22:37:13 +0530

    string type cannot be nil

--- src/test/StringAsserter_test.go
+++ src/test/StringAsserter_test.go
@@ -24,25 +24,3 @@ func TestStringNotEquals(t *testing.T) {
 	sa.IsNotEqualTo("other-string")
 	ft.assertNotFatal(t)
 }
-
-/*
-func TestStringNil(t *testing.T) {
-	ft := &FakeT{}
-	actual := 10
-	pkg.NewAnyAsserter(ft, &actual).IsNil()
-	ft.assertIsFatal(t, fmt.Sprintf("expected '<nil>', got '%v'", &actual))
-	ft.reset()
-	pkg.NewAnyAsserter(ft, nil).IsNil()
-	ft.assertNotFatal(t)
-}
-
-func TestStringNotNil(t *testing.T) {
-	ft := &FakeT{}
-	pkg.NewAnyAsserter(ft, nil).IsNotNil()
-	ft.assertIsFatal(t, "expected 'not <nil>', got '<nil>'")
-	ft.reset()
-	actual := 10
-	pkg.NewAnyAsserter(ft, &actual).IsNotNil()
-	ft.assertNotFatal(t)
-}
-*/
