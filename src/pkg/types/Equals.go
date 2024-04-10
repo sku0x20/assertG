45035018ab897e1c1b13891db83bd6aae4199baa
siddhant <siddhantkumarupmanyu@gmail.com>
2024-04-10 09:57:17 +0530

    refactor

---
+++ src/pkg/types/Equals.go
@@ -0,0 +1,5 @@
+package types
+
+type Equals interface {
+	Equals(other any) bool
+}
--- src/pkg/types/T.go
+++ src/pkg/types/T.go
@@ -3,7 +3,3 @@ package types
 type T interface {
 	Fatalf(format string, args ...any)
 }
-
-type Equals interface {
-	Equals(other any) bool
-}
