c7fdfd19688b40acd12795215762076c58e861e5
siddhant <siddhantkumarupmanyu@gmail.com>
2024-04-23 18:04:57 +0530

    Formatter FormatSame

--- src/pkg/Formatter.go
+++ src/pkg/Formatter.go
@@ -9,6 +9,10 @@ func (f *Formatter) FormatError(actual any, expected any) string {
 	return fmt.Sprintf("Expected:\n%v\nActual:\n%v", expected, actual)
 }
 
+func (f *Formatter) FormatErrorSame(actual string) string {
+	return fmt.Sprintf("Not Expected:\n%v", actual)
+}
+
 func NewFormatter() *Formatter {
 	return &Formatter{}
 }
--- src/test/Formatter_test.go
+++ src/test/Formatter_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 )
 
-func TestFormatErrorInt(t *testing.T) {
+func TestFormatDifferentInt(t *testing.T) {
 	formatter := pkg.NewFormatter()
 	str := formatter.FormatError(1, 2)
 	if str != "Expected:\n2\nActual:\n1" {
@@ -13,10 +13,26 @@ func TestFormatErrorInt(t *testing.T) {
 	}
 }
 
-func TestFormatErrorString(t *testing.T) {
+func TestFormatDifferentString(t *testing.T) {
 	formatter := pkg.NewFormatter()
 	str := formatter.FormatError("1", "2")
 	if str != "Expected:\n2\nActual:\n1" {
+		t.Errorf("wrong formatted string; \n'%s'\n", str)
+	}
+}
+
+func TestFormatDifferent(t *testing.T) {
+	formatter := pkg.NewFormatter()
+	str := formatter.FormatError("1", 2)
+	if str != "Expected:\n2\nActual:\n1" {
+		t.Errorf("wrong formatted string; \n'%s'\n", str)
+	}
+}
+
+func TestFormatSame(t *testing.T) {
+	formatter := pkg.NewFormatter()
+	str := formatter.FormatErrorSame("1")
+	if str != "Not Expected:\n1" {
 		t.Errorf("wrong formatted string; \n'%s'\n", str)
 	}
 }
