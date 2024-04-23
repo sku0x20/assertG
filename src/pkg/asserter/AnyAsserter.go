9c1a08681dbb75216ccc65bbc8406bc6ee6b4465
siddhant <siddhantkumarupmanyu@gmail.com>
2024-04-23 18:25:53 +0530

    AnyAsserter use formatter

--- src/pkg/asserter/AnyAsserter.go
+++ src/pkg/asserter/AnyAsserter.go
@@ -1,39 +1,41 @@
 package asserter
 
 import (
+	"assertG/src/pkg"
 	"assertG/src/pkg/types"
 	"reflect"
 )
 
 type AnyAsserter struct {
-	t      types.T
-	actual any
+	t         types.T
+	actual    any
+	formatter *pkg.Formatter
 }
 
 func (anyA *AnyAsserter) IsEqualTo(expected any) *AnyAsserter {
 	if !anyA.isEqual(expected) {
-		anyA.printError(expected)
+		anyA.mismatch(expected)
 	}
 	return anyA
 }
 
 func (anyA *AnyAsserter) IsNotEqualTo(expected any) *AnyAsserter {
 	if anyA.isEqual(expected) {
-		anyA.printNotError(expected)
+		anyA.notExpected(expected)
 	}
 	return anyA
 }
 
 func (anyA *AnyAsserter) IsNil() *AnyAsserter {
 	if anyA.actual != nil {
-		anyA.printError("<nil>")
+		anyA.mismatch("<nil>")
 	}
 	return anyA
 }
 
 func (anyA *AnyAsserter) IsNotNil() *AnyAsserter {
 	if anyA.actual == nil {
-		anyA.printNotError("<nil>")
+		anyA.notExpected("<nil>")
 	}
 	return anyA
 }
@@ -46,14 +48,16 @@ func (anyA *AnyAsserter) isEqual(expected any) bool {
 	return reflect.DeepEqual(anyA.actual, expected)
 }
 
-func (anyA *AnyAsserter) printNotError(expected any) {
-	anyA.t.Fatalf("not expected '%v',\n got '%v'", expected, anyA.actual)
+func (anyA *AnyAsserter) notExpected(value any) {
+	anyA.t.Fatalf(anyA.formatter.FormatMatch(value))
 }
 
-func (anyA *AnyAsserter) printError(expected any) {
-	anyA.t.Fatalf("expected '%v',\n got '%v'", expected, anyA.actual)
+func (anyA *AnyAsserter) mismatch(expected any) {
+	anyA.t.Fatalf(anyA.formatter.FormatMismatch(anyA.actual, expected))
 }
+
+// todo: convert formatter to explicit dependency
 
 func NewAnyAsserter(t types.T, actual any) *AnyAsserter {
-	return &AnyAsserter{t: t, actual: actual}
+	return &AnyAsserter{t: t, actual: actual, formatter: pkg.NewFormatter()}
 }
