daf78e94fbd273e74eb6b4b6afa36bc5a270eccb
siddhant <siddhantkumarupmanyu@gmail.com>
2024-07-09 01:36:25 +0530

    update gRunner version; fix compilation issue

--- src/test/asserter/AnyAsserterNew_test.go
+++ src/test/asserter/AnyAsserterNew_test.go
@@ -8,11 +8,11 @@ import (
 func Test_AnyAsserterNew(t *testing.T) {
 	r := runner.NewTestsRunner[any](t)
 	r.Setup(func(t2 *testing.T) any { return nil })
-	r.Teardown(func(t2 *testing.T) {})
+	r.Teardown(func(t2 *testing.T, e any) {})
 	r.Add(PrintT)
 	r.Run()
 }
 
-func PrintT(t *testing.T) {
+func PrintT(t *testing.T, e any) {
 	t.Logf("ran")
 }
