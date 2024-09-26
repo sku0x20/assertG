092f8266e7f8fabd592de76e9c7d2e38ba3e987c
siddhant <siddhantkumarupmanyu@gmail.com>
2024-09-26 16:11:45 +0530

    refactor

--- src/test/asserter/Slice_test.go
+++ src/test/asserter/Slice_test.go
@@ -16,34 +16,8 @@ func Test_Slice(t *testing.T) {
 	r.Add(hasLengthPass_Slice)
 	r.Add(hasLengthFail_Slice)
 	r.Add(ContainsExactlyInOrder_Slice)
+	r.Add(ContainsExactlyAnyOrder_Slice)
 	r.Run()
-}
-
-func ContainsExactlyInOrder_Slice(tm *testing.T, _ *assertion.Soft) {
-	type s struct {
-		a int
-		b int
-	}
-	e := []s{{10, 20}, {20, 30}}
-	tests := []struct {
-		a    []s
-		fail bool
-	}{
-		{[]s{{10, 20}, {20, 30}}, false},
-		{[]s{{10, 20}, {20, 10}}, true},
-		{[]s{{10, 20}}, true},
-		{[]s{{20, 30}, {10, 20}}, true},
-	}
-	for _, test := range tests {
-		tm.Run("", func(t *testing.T) {
-			s := assertion.NewSoft(t)
-			a := asserter.NewSlice(s, e)
-			a = a.Contains(asserter.EXACTLY, asserter.IN_ORDER, test.a...)
-			if s.Failed() != test.fail {
-				t.Fatalf("should have failed = %t", test.fail)
-			}
-		})
-	}
 }
 
 func hasLengthPass_Slice(t *testing.T, s *assertion.Soft) {
@@ -61,5 +35,34 @@ func hasLengthFail_Slice(t *testing.T, s *assertion.Soft) {
 	a = a.HasLength(2)
 	if !s.Failed() {
 		t.Fatalf("should have failed")
+	}
+}
+
+type s struct {
+	a int
+	b int
+}
+type test struct {
+	a    []s
+	fail bool
+}
+
+func ContainsExactlyInOrder_Slice(tm *testing.T, _ *assertion.Soft) {
+	e := []s{{10, 20}, {20, 30}}
+	tests := []test{
+		{[]s{{10, 20}, {20, 30}}, false},
+		{[]s{{10, 20}, {20, 10}}, true},
+		{[]s{{10, 20}}, true},
+		{[]s{{20, 30}, {10, 20}}, true},
+	}
+	for _, ct := range tests {
+		tm.Run("", func(t *testing.T) {
+			sa := assertion.NewSoft(t)
+			a := asserter.NewSlice(sa, e)
+			a = a.Contains(asserter.EXACTLY, asserter.IN_ORDER, ct.a...)
+			if sa.Failed() != ct.fail {
+				t.Fatalf("should have failed = %t", ct.fail)
+			}
+		})
 	}
 }
