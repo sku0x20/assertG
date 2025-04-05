98632228ab7da972a80284337852349118b8e521
siddhant <siddhantkumarupmanyu@gmail.com>
2025-04-05 22:57:34 +0530

    remove assert

--- src/test/e2e_test.go
+++ src/test/e2e_test.go
@@ -1,31 +1,38 @@
 package test
 
 import (
-	assertP "github.com/sku0x20/assertG/src/main/assert"
+	"github.com/sku0x20/assertG/src/main/assert"
 	"github.com/sku0x20/assertG/src/main/assert_type"
+	"github.com/sku0x20/assertG/src/main/registry"
 	"github.com/sku0x20/gRunner/src/pkg/runner"
 	"testing"
 )
 
 func Test_WithoutRunner(t *testing.T) {
-	assert := assertP.NewAssert(assert_type.NewHardAssert(t))
+	registry.GlobalRegistrySetAssertType(assert_type.NewHardAssert(t))
 	assert.That(10).
 		IsNotNil().
 		IsEqualTo(10)
 }
 
-func initE(t *testing.T) *assertP.Assert {
-	return assertP.NewAssert(assert_type.NewHardAssert(t))
+func initE(t *testing.T) any {
+	registry.GlobalRegistrySetAssertType(assert_type.NewSoftAssert(t))
+	return nil
 }
 
 func Test_WithRunner(t *testing.T) {
-	r := runner.NewTestsRunner[*assertP.Assert](t, initE)
-	r.Add(makeAssert)
+	r := runner.NewTestsRunner[any](t, initE)
+	r.Add(makeAndValidateAssert)
 	r.Run()
 }
 
-func makeAssert(_ *testing.T, assert *assertP.Assert) {
+func makeAndValidateAssert(t *testing.T, _ any) {
 	assert.That(10).
 		IsNotNil().
-		IsEqualTo(10)
+		IsEqualTo(20)
+
+	sa := registry.GlobalRegistryGetAssertType().(*assert_type.SoftAssert)
+	if !sa.Failed() {
+		t.Fatalf("should have failed")
+	}
 }
