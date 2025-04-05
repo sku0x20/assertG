24ad97fc2389e1f18cf5d4ff963bacf530739ff6
siddhant <siddhantkumarupmanyu@gmail.com>
2025-04-05 17:16:07 +0530

    add IsEqualTo

--- src/pkg/asserter/Slice.go
+++ src/pkg/asserter/Slice.go
@@ -11,134 +11,34 @@ func NewSlice[T any](a assertion.Assertion, val []T) *Slice[T] {
 	return &Slice[T]{a, val}
 }
 
-type Times string
-type Order string
-
-const (
-	EXACTLY  Times = "EXACTLY"
-	ONCE           = "ONCE"
-	MULTIPLE       = "MULTIPLE"
-)
-
-const (
-	IN_ORDER  Order = "IN_ORDER"
-	ANY_ORDER       = "ANY_ORDER"
-)
-
 type Slice[T any] struct {
-	a assertion.Assertion
-	e []T
+	assertion assertion.Assertion
+	actual    []T
 }
 
 func (s *Slice[T]) HasLength(l int) *Slice[T] {
-	if len(s.e) != l {
-		s.a.FailWith(
+	if len(s.actual) != l {
+		s.assertion.FailWith(
 			message.Expected().
-				Value(s.e).
+				Value(s.actual).
 				Verb(verbs.ToHaveLength).
 				Value(l).
 				Verb(verbs.ButWas).
-				Value(len(s.e)),
+				Value(len(s.actual)),
 		)
 	}
 	return s
 }
 
-func (s *Slice[T]) Contains(times Times, order Order, elems ...T) *Slice[T] {
-	switch {
-	case times == EXACTLY && order == IN_ORDER:
-		s.containsExactlyInOrder(elems...)
-	case times == EXACTLY && order == ANY_ORDER:
-		s.containsExactlyInAnyOrder(elems...)
-	case times == ONCE && order == IN_ORDER:
-		s.containsOnceInOrder(elems...)
+func (s *Slice[T]) IsEqualTo(expected []T) *Slice[T] {
+	s.HasLength(len(expected))
+	if !reflect.DeepEqual(s.actual, expected) {
+		s.assertion.FailWith(
+			message.Expected().
+				Value(s.actual).
+				Verb(verbs.ToEqual).
+				Value(expected),
+		)
 	}
 	return s
-}
-
-func (s *Slice[T]) containsExactlyInOrder(elems ...T) {
-	if !reflect.DeepEqual(s.e, elems) {
-		s.a.FailWith(
-			message.Expected().
-				Value(s.e).
-				Verb(verbs.ToContain).
-				Value(elems).
-				Verb(verbs.Exactly).
-				Verb(verbs.InOrder),
-		)
-	}
-}
-
-func (s *Slice[T]) containsExactlyInAnyOrder(elems ...T) {
-	if len(s.e) != len(elems) {
-		s.a.FailWith(
-			message.Expected().
-				Value(s.e).
-				Verb(verbs.ToContain).
-				Value(elems).
-				Verb(verbs.Exactly).
-				Verb(verbs.InAnyOrder),
-		)
-		return
-	}
-	for _, e := range elems {
-		if len(allIndexDeepEqual(s.e, e)) != 1 {
-			s.a.FailWith(
-				message.Expected().
-					Value(s.e).
-					Verb(verbs.ToContain).
-					Value(elems).
-					Verb(verbs.Exactly).
-					Verb(verbs.InAnyOrder),
-			)
-			return
-		}
-	}
-}
-
-func (s *Slice[T]) containsOnceInOrder(elems ...T) {
-	ei := make([]int, 0)
-	for _, e := range elems {
-		indexes := allIndexDeepEqual(s.e, e)
-		if len(indexes) != 1 {
-			s.a.FailWith(
-				message.Expected().
-					Value(s.e).
-					Verb(verbs.ToContain).
-					Value(elems).
-					Verb(verbs.Once).
-					Verb(verbs.InAnyOrder),
-			)
-			return
-		}
-		ei = append(ei, indexes[0])
-	}
-	for i := 1; i < len(ei); i++ {
-		if ei[i] < ei[i-1] {
-			s.a.FailWith(
-				message.Expected().
-					Value(s.e).
-					Verb(verbs.ToContain).
-					Value(elems).
-					Verb(verbs.Once).
-					Verb(verbs.InAnyOrder),
-			)
-		}
-	}
-}
-
-func allIndexDeepEqual[T any](s []T, elem T) []int {
-	return allIndex(s, func(t T) bool {
-		return reflect.DeepEqual(t, elem)
-	})
-}
-
-func allIndex[T any](s []T, f func(t T) bool) []int {
-	in := make([]int, 0, len(s))
-	for i := range s {
-		if f(s[i]) {
-			in = append(in, i)
-		}
-	}
-	return in
 }
--- src/test/asserter/slice/Slice_test.go
+++ src/test/asserter/slice/Slice_test.go
@@ -10,7 +10,7 @@ import (
 func Test_Slice(t *testing.T) {
 	r := runner.NewTestsRunnerEmptyInit[any](t)
 	r.Add(hasLength)
-	//r.Add(ContainsExactlyInOrder_Slice)
+	r.Add(IsEqualTo)
 	//r.Add(ContainsExactlyAnyOrder_Slice)
 	r.Run()
 }
@@ -31,54 +31,36 @@ func hasLength(t *testing.T, _ any) {
 	}
 }
 
-type s struct {
-	a int
-	b int
-}
 type test struct {
-	e    []s
-	a    []s
-	fail bool
+	actual     []int
+	expected   []int
+	shouldFail bool
 }
 
-func ContainsExactlyInOrder_Slice(tm *testing.T, _ *assertion.Soft) {
-	e := []s{{10, 20}, {20, 30}}
+func IsEqualTo(t *testing.T, _ any) {
 	tests := []test{
-		{e, []s{{10, 20}, {20, 30}}, false},
-		{e, []s{{10, 20}, {20, 10}}, true},
-		{e, []s{{10, 20}}, true},
-		{e, []s{{20, 30}, {10, 20}}, true},
+		{[]int{10, 20, 30}, []int{10}, true},
+		{[]int{10, 20, 30}, []int{10, 20, 30, 40}, true},
+		{[]int{10, 20, 30}, []int{30, 10, 20}, true},
+		{[]int{10, 20, 30}, []int{10, 20, 30}, false},
 	}
-	runTests(tm, tests, func(a *asserter.Slice[s], elem ...s) *asserter.Slice[s] {
-		return a.Contains(asserter.EXACTLY, asserter.IN_ORDER, elem...)
-	})
-}
-
-func ContainsExactlyAnyOrder_Slice(tm *testing.T, _ *assertion.Soft) {
-	e := []s{{10, 20}, {20, 30}}
-	tests := []test{
-		{e, []s{{10, 20}, {20, 30}}, false},
-		{e, []s{{10, 20}, {20, 10}}, true},
-		{e, []s{{10, 20}}, true},
-		{e, []s{{20, 30}, {10, 20}}, false},
-	}
-	runTests(tm, tests, func(a *asserter.Slice[s], elem ...s) *asserter.Slice[s] {
-		return a.Contains(asserter.EXACTLY, asserter.ANY_ORDER, elem...)
+	runTests(t, tests, func(a *asserter.Slice[int], expected []int) *asserter.Slice[int] {
+		return a.IsEqualTo(expected)
 	})
 }
 
 func runTests(
 	t *testing.T,
 	tests []test,
-	fnAssertion func(a *asserter.Slice[s], elem ...s) *asserter.Slice[s],
+	fnAssertion func(a *asserter.Slice[int], elem []int) *asserter.Slice[int],
 ) {
 	for _, ct := range tests {
 		t.Run("", func(t *testing.T) {
 			sa := assertion.NewSoft(t)
-			a := asserter.NewSlice(sa, ct.e)
-			a = fnAssertion(a, ct.a...)
-			if sa.Failed() != ct.fail {
-				t.Fatalf("should have failed = %t", ct.fail)
+			a := asserter.NewSlice(sa, ct.actual)
+			a = fnAssertion(a, ct.expected)
+			if sa.Failed() != ct.shouldFail {
+				t.Fatalf("should have failed")
 			}
 		})
 	}
