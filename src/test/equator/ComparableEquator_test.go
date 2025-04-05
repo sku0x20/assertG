4bec8c5b28893c90aa7b36f06a604ff4fed8ac6c
siddhant <siddhantkumarupmanyu@gmail.com>
2025-04-05 17:47:36 +0530

    add ComparableEquator.go

---
+++ src/test/equator/ComparableEquator_test.go
@@ -0,0 +1,25 @@
+package equator
+
+import (
+	"github.com/sku0x20/assertG/src/pkg/equator"
+	"testing"
+)
+
+func Test_ComparableEquator(t *testing.T) {
+	a := 10
+	a2 := 10
+
+	b := struct {
+		a int
+		b string
+		c []string
+	}{10, "10", []string{"20"}}
+
+	rde := equator.NewComparableEquator[int]()
+	if !rde.AreEqual(a, a2) {
+		t.Fatalf("should be equal")
+	}
+	if rde.AreEqual(a, b) {
+		t.Fatalf("should not be equal")
+	}
+}
